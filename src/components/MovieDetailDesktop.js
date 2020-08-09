b78638b7cfbcc8b4718ec1fec8c2cc106f3a39ef
Leandro-Suero <ing.leandrosuero@gmail.com>
2020-08-08 23:17:16 -0300

    style changes in desktop view

--- src/components/MovieDetailDesktop.js
+++ src/components/MovieDetailDesktop.js
@@ -71,7 +71,7 @@ export const MovieDetailDesktop = ({ show, config }) => {
         />
         <div className="w-4/6 z-10 flex flex-col items-center content-center justify-evenly">
           <div className="bg-white bg-opacity-75 flex flex-col rounded">
-            <h1 className="text-center text-4xl font-serif font-bold px-4">
+            <h1 className="text-center text-2xl font-serif font-bold px-4">
               {show.title}
             </h1>
           </div>
@@ -80,7 +80,7 @@ export const MovieDetailDesktop = ({ show, config }) => {
             {show.genres.map((genre) => (
               <span
                 key={genre.id}
-                className="badge-teal inline-block bg-teal-200 text-teal-600 text-xs rounded-full mx-1 px-2 uppercase font-semibold tracking-wide text-base"
+                className="badge-teal inline-block bg-teal-200 text-teal-600 rounded-full mx-1 px-2 uppercase font-semibold tracking-wide text-xs"
               >
                 {genre.name}
               </span>
@@ -107,7 +107,7 @@ export const MovieDetailDesktop = ({ show, config }) => {
                 </span>
               </div>
               <div>
-                <h2 className="text-2xl">Overview</h2>
+                <h2 className="text-xl">Overview</h2>
               </div>
               <div className="">
                 <span className="badge-gray inline-block bg-gray-200 text-gray-800 text-xs rounded-full mx-1 px-2 font-semibold tracking-wide">
@@ -120,7 +120,7 @@ export const MovieDetailDesktop = ({ show, config }) => {
             </div>
             {/* OVERVIEW */}
             <div
-              className="text-center my-2 p-2 text-xs"
+              className="text-center my-2 p-2 text-sm"
               style={{ textShadow: "-1px -1px 4px white" }}
             >
               {show.overview}
@@ -136,10 +136,12 @@ export const MovieDetailDesktop = ({ show, config }) => {
             {show.budget > 0 ? (
               <div className="flex justify-center my-2">
                 <span className="mx-2">
-                  Budget: {moneyFormater.format(show.budget)}
+                  <span className="font-semibold">Budget: </span>
+                  {moneyFormater.format(show.budget)}
                 </span>
                 <span className="mx-2">
-                  Revenue: {moneyFormater.format(show.revenue)}
+                  <span className="font-semibold">Revenue: </span>
+                  {moneyFormater.format(show.revenue)}
                 </span>
               </div>
             ) : null}
@@ -151,7 +153,7 @@ export const MovieDetailDesktop = ({ show, config }) => {
                 href={show.homepage}
                 target="_blank"
                 rel="noopener noreferrer"
-                className="inline-block text-center bg-teal-500 text-white text-md rounded-full py-2 px-6 uppercase font-semibold text-xl"
+                className="inline-block text-center bg-teal-500 text-white text-md rounded-full py-2 px-6 uppercase font-semibold text-base"
                 style={{ animation: "pulse-light 2s infinite" }}
               >
                 VISIT HOMEPAGE
--- src/components/SerieDetailDesktop.js
+++ src/components/SerieDetailDesktop.js
@@ -68,7 +68,7 @@ export const SerieDetailDesktop = ({ show, config }) => {
         />
         <div className="w-4/6 z-10 flex flex-col items-center content-center justify-evenly">
           <div className="bg-white bg-opacity-75 flex flex-col rounded">
-            <h1 className="text-center text-4xl font-serif font-bold px-4">
+            <h1 className="text-center text-2xl font-serif font-bold px-4">
               {show.name}
             </h1>
           </div>
@@ -77,7 +77,7 @@ export const SerieDetailDesktop = ({ show, config }) => {
             {show.genres.map((genre) => (
               <span
                 key={genre.id}
-                className="badge-teal inline-block bg-teal-200 text-teal-600 text-xs rounded-full mx-1 px-2 uppercase font-semibold tracking-wide text-base"
+                className="badge-teal inline-block bg-teal-200 text-teal-600 rounded-full mx-1 px-2 uppercase font-semibold tracking-wide text-xs"
               >
                 {genre.name}
               </span>
@@ -108,7 +108,7 @@ export const SerieDetailDesktop = ({ show, config }) => {
                 </span>
               </div>
               <div>
-                <h2 className="text-2xl">Overview</h2>
+                <h2 className="text-xl">Overview</h2>
               </div>
               <div className="">
                 <span className="badge-gray inline-block bg-gray-200 text-gray-800 text-xs rounded-full m-1 px-2 font-semibold tracking-wide">
@@ -122,7 +122,7 @@ export const SerieDetailDesktop = ({ show, config }) => {
             </div>
             {/* OVERVIEW */}
             <div
-              className="text-center my-2 p-2 text-xs"
+              className="text-center my-2 p-2 text-sm"
               style={{ textShadow: "-1px -1px 4px white" }}
             >
               {show.overview}
@@ -135,7 +135,7 @@ export const SerieDetailDesktop = ({ show, config }) => {
                 href={show.homepage}
                 target="_blank"
                 rel="noopener noreferrer"
-                className="inline-block text-center bg-teal-500 text-white text-md rounded-full py-2 px-6 uppercase font-semibold text-xl"
+                className="inline-block text-center bg-teal-500 text-white text-md rounded-full py-2 px-6 uppercase font-semibold text-base"
                 style={{ animation: "pulse-light 2s infinite" }}
               >
                 VISIT HOMEPAGE
